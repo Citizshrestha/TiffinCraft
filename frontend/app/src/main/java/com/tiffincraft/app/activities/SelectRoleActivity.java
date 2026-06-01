bf4b13573fe58742e9b450f5cd25e7a5b35decd2
citizshrestha <citizshresthaa@gmail.com>
2026-06-01 19:43:18 +0545

    Select Role Activity added

---
+++ frontend/app/src/main/java/com/tiffincraft/app/activities/SelectRoleActivity.java
@@ -0,0 +1,12 @@
+package com.tiffincraft.app.activities;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SelectRoleActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // layout coming next
+    }
+}
