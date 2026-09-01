a054561e940fa4bf15198680a3d70239e961d4af
citizshrestha <citizshresthaa@gmail.com>
2026-09-01 12:44:30 +0545

    Update ApiService

--- frontend/app/src/main/java/com/tiffincraft/app/api/ApiService.java
+++ frontend/app/src/main/java/com/tiffincraft/app/api/ApiService.java
@@ -58,6 +58,7 @@ import retrofit2.Call;
 import retrofit2.http.Body;
 import retrofit2.http.DELETE;
 import retrofit2.http.GET;
+import retrofit2.http.HTTP;
 import retrofit2.http.Header;
 import retrofit2.http.Multipart;
 import retrofit2.http.POST;
